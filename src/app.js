41c1b7fa378f2149b6cee1041221c767e0f8ed9f
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-15 12:14:33 +0530

    Implement mongoDB connection and create initial code logics in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
-console.log("E Commerce API");
+require('dotenv').config();
+require('express-async-errors');
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+
+const express = require('express');
+const app = express();
+const connectDB = require("./db/connect");
+
+const notFoundMiddleware = require('./middlewares/notFoundMiddleware');
+const errorHandlerMiddleware = require('./middlewares/errorhandlerMiddleware');
+const authenticationMiddleware = require("./middlewares/authenticationMiddleware");
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB();
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start().then(r => {});
