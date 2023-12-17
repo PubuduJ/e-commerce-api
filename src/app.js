ee7f2fa0e993a8ed1e44e11304abd5a2cdc28aca
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-17 13:18:24 +0530

    Implement product upload logic

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
+const fileUpload = require("express-fileupload");
+// USE VERSION AS V2.
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.CLOUD_API_KEY,
+    api_secret:process.env.CLOUD_API_SECRET,
+})
 
 // extra security packages
 const helmet = require("helmet");
@@ -30,6 +38,8 @@ app.use(cors());
 app.use(xss());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(morgan("tiny"));
+// temporary files (store in tmp directory) are used to handle the image upload.
+app.use(fileUpload({useTempFiles: true}));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticationMiddleware, userRouter);
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,8 @@
 const { StatusCodes } = require('http-status-codes');
 const Product = require("../models/Product");
 const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 const createProduct = async (req, res) => {
     // attach logged in user id to the user property in Product document.
@@ -40,7 +42,7 @@ const deleteProduct = async (req, res) => {
     const {id: productId} = req.params;
 
     const product = await Product.findByIdAndDelete({_id:productId});
-    
+
     if (!product) {
         throw new CustomError.NotFoundError(`No product with id : ${productId}`);
     }
@@ -48,7 +50,28 @@ const deleteProduct = async (req, res) => {
 }
 
 const uploadProductImage = async (req, res) => {
-    res.send("upload product image");
+    // Check if file exists.
+    if (!req.files) {
+        throw new CustomError.BadRequestError("No file uploaded");
+    }
+    const productImage = req.files.image;
+    // Check the format.
+    if (!productImage.mimetype.startsWith("image")) {
+        throw new CustomError.BadRequestError("Please upload an image");
+    }
+    // Check the size of the image ( < 1MB).
+    const maxSize = 1024 * 1024;
+    if (productImage.size > maxSize) {
+        throw new CustomError.BadRequestError("Please upload an image smaller than 1MB");
+    }
+    // upload the image to cloudinary and get the secure url.
+    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+        use_filename: true,
+        folder: "file-upload"
+    })
+    // once upload file to the cloudinary, remove the file from tmp directory.
+    fs.unlinkSync(req.files.image.tempFilePath);
+    return res.status(StatusCodes.OK).json({imageURL: result.secure_url});
 }
 
 module.exports = {
