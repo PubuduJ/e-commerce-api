20c3a4294d31d6210261788e09c45b7847247e32
Pubudu Janith <pubudujanith123@gmail.com>
2023-12-22 10:39:15 +0530

    Improvements

--- src/models/Review.js
+++ src/models/Review.js
@@ -31,6 +31,23 @@ const ReviewSchema = new mongoose.Schema({
 
 // Set a compound index.
 // Only one review per product per user.
-ReviewSchema.index({product:1, user:1}, {unique: true})
+ReviewSchema.index({product:1, user:1}, {unique: true});
+
+// static methods are used with schema.
+ReviewSchema.statics.calculateAverageRating = async function(productId) {
+    console.log(productId.toString());
+}
+
+ReviewSchema.post("save", async function() {
+    await this.constructor.calculateAverageRating(this.product);
+
+    // inorder to invoke this we have to use review.save() method.
+    console.log("Post save hook called");
+})
+
+ReviewSchema.post("remove", async function() {
+    // inorder to invoke this we have to use review.remove() method.
+    console.log("Post remove hook called");
+})
 
 module.exports = mongoose.model("Review", ReviewSchema);
